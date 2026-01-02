0946bf5b786cf103518a80ed96f895aaff44c581
haneshi <wackymob98@naver.com>
2026-01-02 10:07:26 +0900

    객실 테이블 마이그레이션

--- database/migrations/2025_12_31_114426_pension_room.php
+++ database/migrations/2025_12_31_114426_pension_room.php
@@ -12,6 +12,27 @@ return new class extends Migration
     public function up(): void
     {
         //
+        Schema::create('pension_room', function (Blueprint $table) {
+            $table->id()->comment('객실 고유값');
+            $table->smallInteger('seq')->default(9999)->comment('순서');
+            $table->unsignedBigInteger('pension_id')->comment('펜션고유값');
+            $table->string('name')->comment('객실 이름');
+
+            $table->integer('area1')->comment('객실 면적(㎡)');
+            $table->integer('area2')->comment('객실 평');
+
+            $table->string('shape')->comment('객실 유형');
+
+            $table->json('jsonData')->comment('구비시설');
+
+            $table->string('etc')->comment('특이사항');
+            $table->integer('person_min')->comment('기준인원');
+            $table->integer('person_max')->comment('최대인원');
+            $table->boolean('is_active')->default(true)->comment('사용유무');
+            $table->timestamps();
+
+            $table->comment('펜션 객실');
+        });
     }
 
     /**
@@ -20,5 +41,6 @@ return new class extends Migration
     public function down(): void
     {
         //
+        Schema::dropIfExists('pension_room');
     }
 };
