d959a197f4683edaa09020353a0d08dd23747078
haneshi <wackymob98@naver.com>
2026-01-14 14:52:01 +0900

    faq 게시글 수정

---
+++ resources/views/admin/pages/board/faq/partials/form/edit.blade.php
@@ -0,0 +1,110 @@
+@section('afterStyle')
+    <link rel="stylesheet" href="{{ asset('assets/plugins/ckeditor/css/ckeditor.min.css') }}">
+    @if ($board->is_period)
+        <link href="{{ asset('assets/plugins/litepicker/css/litepicker.min.css') }}" rel="stylesheet" />
+    @endif
+@endsection
+<div class="row">
+    <div class="flex-fill">
+        <div class="card">
+            <div class="card-header pb-0">
+                <div class="d-flex align-items-center">
+                    <p class="mb-0">{{ $pageData['title'] }} 추가</p>
+                </div>
+            </div>
+            <div class="card-body">
+                <form id="frm" autocomplete="off" novalidate>
+                    <input type="hidden" name="pType" value="setBoardPost">
+                    <input type="hidden" name="id" value="{{ $data->id }}">
+                    <div class="row g-3 mb-3">
+                        <div class="col-md">
+                            <label for="content" class="form-label">질문</label>
+                            <textarea class="form-control" id="content" name="content">{{ $data->content }}</textarea>
+                        </div>
+                    </div>
+                    <div class="row g-3 mb-3">
+                        <div class="col-md">
+                            <label for="content_sub" class="form-label">답변</label>
+                            <textarea class="form-control" id="content_sub" name="content_sub">{{ $data->content_sub }}</textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input ms-auto mt-1" type="checkbox" id="is_active"
+                                    name="is_active" @if ($data->is_active) checked @endif>
+                                <label class="form-check-label ms-2" for="is_active">사용유무</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-end gap-2">
+                        <a href="{{ route('admin.board', ['board_name' => $board->board_name] + $paramData) }}"
+                            class="btn btn-outline-secondary">목록으로</a>
+                        <button id="submitBtn" type="submit"
+                            class="btn btn bg-gradient-primary">{{ $board->board_name }} 작성</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+@section('afterScript')
+    <script src="{{ asset('assets/plugins/ckeditor/js/ckeditor.js') }}"></script>
+    <script src="{{ asset('assets/plugins/ckeditor/js/editor.js') }}"></script>
+    <script src="{{ asset('assets/plugins/validation/just-validate.min.js') }}"></script>
+
+    @if ($board->is_period)
+        <script src="{{ asset('assets/plugins/litepicker/js/litepicker.min.js') }}"></script>
+    @endif
+    <script>
+        setEditor.ckeditor.classic('#content');
+        setEditor.ckeditor.classic('#content_sub');
+
+        @if ($board->is_period)
+            common.litepicker('start_date');
+            common.litepicker('end_date');
+        @endif
+
+        // CKEditor 내용 검증 함수
+        // CkEditor 빈값으로 넣으면 <p>&nbsp;</p> 이 값으로 들어가서 유효성 검사를 통과해버린다
+        // 아래 함수로 빈값을 추출하는 값으로 검증한다
+        const validateEditorContent = (value) => {
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = value;
+            const text = tempDiv.textContent || tempDiv.innerText || '';
+            return text.trim().length > 0;
+        };
+
+        const procAddValidator = new JustValidate('#frm', apps.plugins.JustValidate.basic());
+        procAddValidator
+            .onSuccess((e) => {
+                e.preventDefault();
+                const form = document.getElementById('frm');
+                const formData = new FormData(form);
+
+                common.ajax.postFormData('{{ route('admin.board.data', $board->board_name) }}', formData);
+            })
+        @if ($board->is_period)
+            .addField('#end_date', [{
+                validator: (value, context) => {
+                    const startDate = document.getElementById('start_date').value;
+                    const endDate = value;
+
+                    if (!startDate || !endDate) return true;
+
+                    return startDate < endDate;
+                },
+                errorMessage: '시작일보다 종료일이 늦어야 합니다.',
+            }])
+        @endif
+        .addField('#content', [{
+                validator: validateEditorContent,
+                errorMessage: '질문을 입력해 주세요!'
+            }])
+            .addField('#content_sub', [{
+                validator: validateEditorContent,
+                errorMessage: '답변을 입력해 주세요!',
+            }]);
+    </script>
+@endsection
