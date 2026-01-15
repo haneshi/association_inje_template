f1c1bd0de9449b50264d13859d2068d8f046ae66
haneshi <wackymob98@naver.com>
2026-01-15 15:03:07 +0900

    !!! 중요함! 페이지 네이션 1-10, 11-20 스타일로 바꿈 !!! 이전 버전은 라라벨 기본 스타일 (보기 편리하기함)

--- resources/views/admin/components/pagination.blade.php
+++ resources/views/admin/components/pagination.blade.php
@@ -1,42 +1,46 @@
 @if ($paginator->hasPages())
     <ul class="pagination m-0">
-        @if ($paginator->onFirstPage())
+        @php
+            $current = $paginator->currentPage();
+            $last = $paginator->lastPage();
+            $perGroup = 10; // 한 그룹당 10개씩
+            $currentGroup = ceil($current / $perGroup);
+            $start = ($currentGroup - 1) * $perGroup + 1;
+            $end = min($start + $perGroup - 1, $last);
+        @endphp
+
+        {{-- 이전 그룹 버튼 --}}
+        @if ($currentGroup > 1)
+            <li class="page-item">
+                <a class="page-link" href="{{ $paginator->url($start - 1) }}" tabindex="-1">
+                    <x-tabler-chevron-left class="icon" />
+                </a>
+            </li>
+        @else
             <li class="page-item disabled">
                 <a class="page-link" href="javascript:void(0)" tabindex="-1" aria-disabled="true">
                     <x-tabler-chevron-left class="icon" />
                 </a>
             </li>
-        @else
-            <li class="page-item">
-                <a class="page-link" href="{{ $paginator->previousPageUrl() }}" tabindex="-1">
-                    <x-tabler-chevron-left class="icon" />
-                </a>
-            </li>
         @endif
 
-        @foreach ($elements as $element)
-            @if (is_string($element))
-                <li class="page-item disabled">{{ $element }}</li>
+        {{-- 페이지 번호들 (1~10, 11~20 형식) --}}
+        @for ($page = $start; $page <= $end; $page++)
+            @if ($page == $current)
+                <li class="page-item active">
+                    <a class="page-link">{{ $page }}</a>
+                </li>
+            @else
+                <li class="page-item">
+                    <a class="page-link" href="{{ $paginator->url($page) }}">{{ $page }}</a>
+                </li>
             @endif
+        @endfor
 
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <li class="page-item active">
-                            <a class="page-link">{{ $page }}</a>
-                        </li>
-                    @else
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $url }}">{{ $page }}</a>
-                        </li>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
-        @if ($paginator->hasMorePages())
+        {{-- 다음 그룹 버튼 --}}
+        @if ($end < $last)
             <li class="page-item">
-                <a class="page-link" href="{{ $paginator->nextPageUrl() }}" tabindex="-1">
+                <a class="page-link" href="{{ $paginator->url($end + 1) }}" tabindex="-1">
                     <x-tabler-chevron-right class="icon" />
                 </a>
             </li>
